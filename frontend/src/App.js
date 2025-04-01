08f290cf58f73f50e687d4cedcf1fa1f8255ce7f
junjianni <junjian.ni@syngenta.com>
2025-04-01 14:12:11 -0400

    first api call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
 
-
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_ACCESS_KEY;
 
 function App() {
   const [word, setWord] = useState('');
@@ -11,9 +11,16 @@ function App() {
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
+    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
   }
 
-  
   return (
     <div>
       <Header title="Images Gallery"/>  
