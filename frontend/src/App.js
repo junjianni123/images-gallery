b3d7ec274f0bf87893141e053497ebc33c23c11c
junjianni <junjian.ni@syngenta.com>
2025-04-12 15:45:37 -0400

    welcome component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Search from './components/Search';
 import React from 'react';
 import ImageCard from './components/ImageCard';
+import Welcome from './components/Welcome';
 import { Container, Row, Col } from 'react-bootstrap';
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_ACCESS_KEY;
@@ -34,13 +35,15 @@ function App() {
       <Header title="Images Gallery"/>  
       <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
       <Container className="mt-4">
-        <Row xs={1} md={2} lg={3} >
-          {images.map((image, i) => (
-            <Col key={i} className='pb-3'>
-              <ImageCard image={image} deleteImage={handleDeleteImage} />
-            </Col>
-        ))}
-        </Row>
+        {images.length ? (
+          <Row xs={1} md={2} lg={3} >
+            {images.map((image, i) => (
+              <Col key={i} className='pb-3'>
+                <ImageCard image={image} deleteImage={handleDeleteImage} />
+              </Col>
+          ))}
+          </Row>
+        ) : <Welcome />}
       </Container>
 
      </div>
